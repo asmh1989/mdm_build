eaa0f9360b4ad9af72bd2c5feb0e9a2129e3d673
sunmh <sunmh@justsy.com>
2019-02-15 18:01:35 +0800

    app icon 自动尺寸生成

---
+++ bin/lib/framework/create_icon.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+import 'package:image/image.dart';
+import '../utils.dart';
+
+class CreateIcon{
+
+  static Map<String, int> icons = {
+    'drawable-mdpi': 64,
+    'drawable-hdpi': 36,
+    'drawable-xhdpi': 96,
+    'drawable-xxhdpi': 144,
+    'drawable-xxxhdpi': 192,
+  };
+
+  static void create(String file, String path) async {
+    Utils.log('start decode $file');
+    Image image = decodeImage(await new File(file).readAsBytes());
+    Utils.log('decode $file done');
+
+    for(String  key in icons.keys){
+      String dir ='$path/$key';
+      Directory(dir).createSync(recursive: true);
+      File file =  new File('$dir/auto_build_icon.png');
+
+      Utils.log('start new ${file.path}');
+
+      Image thumbnail = copyResize(image, icons[key]);
+
+      await file.writeAsBytes(encodePng(thumbnail));
+      Utils.log('done new ${file.path}');
+
+    }
+  }
+
+
+}
--- bin/lib/framework/mdm4_framework.dart
+++ bin/lib/framework/mdm4_framework.dart
@@ -7,6 +7,7 @@ import '../db.dart';
 import '../constant.dart';
 import '../model/build_model.dart';
 import 'package:shell/shell.dart';
+import 'create_icon.dart';
 
 class MDM4Framework implements BaseFramework {
 
@@ -41,7 +42,16 @@ class MDM4Framework implements BaseFramework {
     if(result.exitCode != 0){
       throw new Exception('copySrc error: ${result.exitCode}');
     }
+  }
 
+  void changeRes(BuildModel model, String path, String source) async {
+    String appIcon = '$path/appicon.png';
+
+
+    if (model.params.app_info.app_icon.isNotEmpty) {
+      await Utils.download(model.params.app_info.app_icon, appIcon);
+      CreateIcon.create(appIcon, '${source}/app/src/main/res');
+    }
   }
 
   @override
@@ -50,22 +60,24 @@ class MDM4Framework implements BaseFramework {
 
     String appPath = '${Utils.cachePath}/${model.build_id}';
 
-    String appIcon = '${appPath}/appicon.png';
     String gitSrc = 'template-mdm';
     String templatePath = '/home/sun/.mdm_build/b5b84460-0b50-11e9-9947-b7139caa41b8/template-mdm';//'$appPath/$gitSrc';
 
     try {
-//      if (params.app_info.app_icon.isNotEmpty) {
-//        await Utils.download(params.app_info.app_icon, appIcon);
-//      }
-//
-//      await Utils.clone(url: 'ssh://git@192.168.2.34:8442/sunmh/mdm_build.git',
-//          path: appPath,
-//          branch: getName(),
-//          name: gitSrc);
 
-//      await prepare(model, templatePath);
+      /// mdm_4 需要在as工程下进行编译, 所以需要先下载模板
+      await Utils.clone(url: 'ssh://git@192.168.2.34:8442/sunmh/mdm_build.git',
+          path: appPath,
+          branch: getName(),
+          name: gitSrc);
 
+      /// 准备工作, 下载实际的svn代码, 并把需要的代码合并到模板工程中
+      await prepare(model, templatePath);
+
+      ///  修改app icon
+      await changeRes(model, appPath, templatePath);
+
+      /// 修改配置
 
     } catch(e){
       model.status = BuildStatus.newFailed(e.toString());
