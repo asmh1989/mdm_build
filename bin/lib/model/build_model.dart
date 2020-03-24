27b5ecdcc4e0a7bb5d17db6a9be30e09bb6318df
sun <sunmh@justsafe.com>
2020-03-24 17:36:32 +0800

    数据库中时间显示修改

--- bin/lib/model/build_model.dart
+++ bin/lib/model/build_model.dart
@@ -38,7 +38,7 @@ class BuildModel {
 
   Map<String, dynamic> toJson() {
     var data = <String, dynamic>{};
-    data['date'] = date;
+    data['date'] = date.toIso8601String();
     data[propBuildId] = build_id;
     data[propCode] = status?.code;
     data[propMsg] = status?.msg;
